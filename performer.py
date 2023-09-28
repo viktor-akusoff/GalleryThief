2b46e3029bb302614cba42229dbbc4bcd48a1063
viktor-akusoff <kirill.voloshin.98@ya.ru>
2023-09-28 16:31:52 +0300

    implementing performer Thief

---
+++ performer.py
@@ -0,0 +1,22 @@
+from strategies import StealingStrategy
+
+
+class Thief():
+
+    def __init__(self, strategy: StealingStrategy):
+        self._strategy: StealingStrategy = strategy
+
+    @property
+    def strategy(self) -> StealingStrategy:
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, strategy):
+        self._strategy = strategy
+
+    def get_images_list(self, *args):
+        result = {}
+        for a in args:
+            res = self._strategy.get_images(a[0], a[1])
+            result[a[0]] = res
+        return result
